023cc70d052e03f492d2e8ce025121a2bd3da44d
EC2 Default User <ec2-user@ip-172-31-46-212.ap-northeast-1.compute.internal>
2020-03-30 12:29:58 +0000

    finish static pages

---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,17 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+ 
+  test "should get home" do
+    get static_pages_home_url
+    assert_response :success
+    assert_select "title", "Home | Insta App"
+  end
+  
+  test "should get about" do
+    get static_pages_about_url
+    assert_response :success
+    assert_select "title", "About | Insta App"
+  end
+
+end
