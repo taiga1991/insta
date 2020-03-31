4ca1a743e92f0e90e4636a1e224b39631ee9c39b
EC2 Default User <ec2-user@ip-172-31-46-212.ap-northeast-1.compute.internal>
2020-03-31 14:04:05 +0000

    finish layout and routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,15 +3,21 @@ require 'test_helper'
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
  
   test "should get home" do
-    get static_pages_home_url
+    get root_path
     assert_response :success
-    assert_select "title", "Home | Insta App"
+    assert_select "title", "Insta App"
   end
   
   test "should get about" do
-    get static_pages_about_url
+    get about_path
     assert_response :success
     assert_select "title", "About | Insta App"
+  end
+  
+  test "should get contact" do
+    get contact_path
+    assert_response :success
+    assert_select "title", "Contact | Insta App"
   end
 
 end
