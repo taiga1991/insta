843e9f0df808b7e49c7a42df0aecca289b6a0871
EC2 Default User <ec2-user@ip-172-31-46-212.ap-northeast-1.compute.internal>
2020-04-05 14:46:57 +0000

    finish user microposts

---
+++ app/uploaders/picture_uploader.rb
@@ -0,0 +1,19 @@
+class PictureUploader < CarrierWave::Uploader::Base
+   include CarrierWave::MiniMagick
+  process resize_to_limit: [400, 400]
+
+   if Rails.env.production?
+    storage :fog
+   else
+    storage :file
+   end
+  
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  # アップロード可能な拡張子のリスト
+  def extension_whitelist
+    %w(jpeg png)
+  end
+end
